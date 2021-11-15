7fc733f47cc86f5bd1335ee583c60c00c0402a2e
Raha Mohebbi <rahamohebbi@gmail.com>
2021-11-15 13:07:21 -0800

    fix for routing issue where going /page1 would not work. changed router to use HashRouter instead

--- my-app-typescript/src/index.tsx
+++ my-app-typescript/src/index.tsx
@@ -4,20 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Page1 from './Page1';
 import Page2 from './Page2';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} >
           <Route path="page1" element={<Page1 />} />
           <Route path="page2" element={<Page2 />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
